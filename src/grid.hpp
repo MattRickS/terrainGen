b1e5920a8bdc8daf22fcbb37462e5bc22984e3aa
MattRickS <mattalexshaw@gmail.com>
2021-06-09 20:50:11 +0100

    Cell finder utilties

--- src/grid.hpp
+++ src/grid.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <algorithm>
 #include <memory>
 #include <vector>
 #include <vec3.hpp>
@@ -7,11 +8,26 @@ template <typename T>
 struct Cell
 {
     std::unique_ptr<Cell> below = nullptr;
-    float depth;
+    float depth = 0.0f;
     T value;
 
     Cell() {}
     Cell(float d, T val) : depth(d), value(val) {}
+
+    float totalDepth()
+    {
+        float d{depth};
+        if (below != nullptr)
+            d += below->totalDepth();
+        return d;
+    }
+    size_t numLayers()
+    {
+        size_t count{1};
+        if (below != nullptr)
+            count += below->numLayers();
+        return count;
+    }
 };
 
 template <typename T>
@@ -21,19 +37,18 @@ public:
     std::vector<std::unique_ptr<Cell<T>>> values;
 
     Grid2D(size_t w, size_t h) : m_width(w), m_height(h), values(w * h) {}
-    // Grid2D(size_t w, size_t h, float depth, T val) : Grid2D(w, h)
-    // {
-    //     fill(depth, val);
-    // }
 
-    // void fill(float depth, T val)
-    // {
-    //     for (size_t y = 0; y < m_height; ++y)
-    //         for (size_t x = 0; x < m_height; ++x)
-    //             addDepth(x, y, depth, val);
-    // }
+    void fill(float depth, T val)
+    {
+        for (size_t y = 0; y < m_height; ++y)
+            for (size_t x = 0; x < m_height; ++x)
+                addDepth(x, y, depth, val);
+    }
     void addDepth(size_t x, size_t y, float depth, T val)
     {
+        if (depth <= 0.0f)
+            return;
+
         auto origCell = cell(x, y);
         if (origCell == nullptr)
         {
@@ -54,17 +69,21 @@ public:
     const size_t width() const { return m_height; };
     const size_t height() const { return m_width; };
     const size_t size() const { return m_width * m_height; };
-    // const size_t depth(size_t x, size_t y) const
-    // {
-    //     float d = 0.0f;
-    //     auto currCell = cell(x, y);
-    //     while (currCell != nullptr)
-    //     {
-    //         d += currCell.depth;
-    //         currCell = currCell.below;
-    //     }
-    //     return d;
-    // };
+
+    Cell<T> *deepestCell() const
+    {
+        auto it = std::max_element(values.begin(), values.end(), [](const auto &lhs, const auto &rhs) -> bool
+                                   { lhs->totalDepth() < rhs->totalDepth(); });
+        return (*it).get();
+    }
+
+    Cell<T> *mostLayeredCell() const
+    {
+        auto it = std::max_element(values.begin(), values.end(), [](const auto &lhs, const auto &rhs) -> bool
+                                   { lhs->numLayers() < rhs->numLayers(); });
+        return (*it).get();
+    }
+
     template <typename U>
     bool isValidPos(const U x, const U y)
     {
