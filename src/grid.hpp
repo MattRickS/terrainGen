f337a06de5fe15a4999bf94a64721719ee3fb3ac
MattRickS <mattalexshaw@gmail.com>
2021-06-12 11:44:17 +0100

    Iterator style

--- src/grid.hpp
+++ src/grid.hpp
@@ -34,10 +34,10 @@ struct Cell
 template <typename T>
 class Grid2D
 {
-public:
-    std::vector<std::unique_ptr<Cell<T>>> values;
+    using CellIterator = typename std::vector<Cell<T>>::iterator;
 
-    Grid2D(size_t w, size_t h) : m_width(w), m_height(h), values(w * h) {}
+public:
+    Grid2D(size_t w, size_t h) : m_width(w), m_height(h), m_values(w * h) {}
 
     void fill(float depth, T val)
     {
@@ -45,73 +45,60 @@ public:
             for (size_t x = 0; x < m_width; ++x)
                 addDepth(x, y, depth, val);
     }
-    void addDepth(size_t x, size_t y, float depth, T val)
-    {
-        if (depth <= 0.0f)
-            return;
-
-        auto origCell = cell(x, y);
-        if (origCell == nullptr)
-        {
-            values[y * m_width + x] = std::make_unique<Cell<T>>(depth, val);
-        }
-        else if (origCell->depth <= 0)
-        {
-            origCell->value = val;
-            origCell->depth = depth;
-        }
-        else
-        {
-            auto newCell = std::make_unique<Cell<T>>(depth, val);
-            newCell->below = std::move(values[y * m_width + x]);
-            values[y * m_width + x] = std::move(newCell);
-        }
-    }
-    void removeDepth(size_t x, size_t y, float depth)
-    {
-        auto *topCell = cell(x, y);
-        if (topCell == nullptr)
-        {
-            return;
-        }
-        else if (depth > topCell->depth)
-        {
-            // Remove cell and recurse with remaining depth
-            float remainingDepth = depth - topCell->depth;
-            values[y * m_width + x] = topCell->below;
-            removeDepth(x, y, remainingDepth);
-        }
-        else
-        {
-            topCell->depth -= depth;
-        }
-    }
     const size_t width() const { return m_height; };
     const size_t height() const { return m_width; };
     const size_t size() const { return m_width * m_height; };
-
-    Cell<T> *deepestCell() const
+    void addDepth(CellIterator it, float depth, T val)
     {
-        auto it = std::max_element(values.begin(), values.end(), [](const auto &lhs, const auto &rhs) -> bool
-                                   { lhs->totalDepth() < rhs->totalDepth(); });
-        return (*it).get();
-    }
+        // assert(it != end(), "Iterator out of range");
+        // assert(depth > 0.0f, "Can't add negative depth");
+        if (it == end())
+            throw("Out of bounds");
+        else if (depth < 0.0f)
+            throw("Cannot add negative depth");
 
-    Cell<T> *mostLayeredCell() const
+        // Replace the Cell contents if it's not got any depth (default)
+        if (it->depth <= 0.0f)
+        {
+            *it = Cell<T>(depth, val);
+        }
+        else
+        {
+            // Copy the current cell into a new child cell, update the current cell
+            auto oldCell = std::make_unique<Cell<T>>(std::move(*it));
+            *it = Cell<T>(depth, val);
+            it->below = std::move(oldCell);
+        }
+    }
+    void removeDepth(CellIterator it, float depth)
     {
-        auto it = std::max_element(values.begin(), values.end(), [](const auto &lhs, const auto &rhs) -> bool
-                                   { lhs->numLayers() < rhs->numLayers(); });
-        return (*it).get();
+        // TODO: Handle negative depth
+        if (it == end())
+        {
+            throw("Out of bounds");
+        }
+        else if (depth > it->depth)
+        {
+            // Remove cell and recurse with remaining depth
+            float remainingDepth = depth - it->depth;
+            // move the unique pointer contents out into the vector. The vector now manages
+            // the memory, and unique ptr cleans up without removing anything important.
+            *it = std::move(*(it->below));
+            removeDepth(it, remainingDepth);
+        }
+        else
+        {
+            it->depth -= depth;
+        }
     }
-
     vec3f normal(size_t x, size_t y)
     {
         // Central differential
         // When calculating out of bounds values, assume the edge values are extended
-        float left = values[y * m_width + (x > 0 ? x - 1 : x)]->totalDepth();
-        float right = values[y * m_width + (x < m_width - 1 ? x + 1 : x)]->totalDepth();
-        float top = values[(y > 0 ? y - 1 : y) * m_width + x]->totalDepth();
-        float bottom = values[(y < m_height - 1 ? y + 1 : y) * m_width + x]->totalDepth();
+        float left = m_values[y * m_width + (x > 0 ? x - 1 : x)].totalDepth();
+        float right = m_values[y * m_width + (x < m_width - 1 ? x + 1 : x)].totalDepth();
+        float top = m_values[(y > 0 ? y - 1 : y) * m_width + x].totalDepth();
+        float bottom = m_values[(y < m_height - 1 ? y + 1 : y) * m_width + x].totalDepth();
         return normalise(vec3f(2.0f * (right - left), 2.0f * (bottom - top), -4.0f));
     }
 
@@ -120,21 +107,31 @@ public:
     {
         return (0 <= x && x < m_width && 0 <= y && y < m_height);
     }
-    Cell<T> *cell(size_t x, size_t y) { return values[y * m_width + x].get(); }
 
-    // std::vector<std::unique_ptr<Cell<T>>>::iterator begin()
-    // {
-    //     return values.begin();
-    // }
-    // std::vector<std::unique_ptr<Cell<T>>>::iterator cellIndex(size_t x, size_t y)
-    // {
-    //     return values.begin() + y * m_width + x;
-    // }
-    // std::vector<std::unique_ptr<Cell<T>>>::iterator end()
-    // {
-    //     return values.end();
-    // }
+    CellIterator begin()
+    {
+        return m_values.begin();
+    }
+    CellIterator cellIndex(size_t x, size_t y)
+    {
+        return m_values.begin() + y * m_width + x;
+    }
+    CellIterator end()
+    {
+        return m_values.end();
+    }
+    CellIterator deepestCell() const
+    {
+        return std::max_element(begin(), end(), [](const auto &lhs, const auto &rhs) -> bool
+                                { lhs->totalDepth() < rhs->totalDepth(); });
+    }
+    CellIterator mostLayeredCell() const
+    {
+        return std::max_element(begin(), end(), [](const auto &lhs, const auto &rhs) -> bool
+                                { lhs->numLayers() < rhs->numLayers(); });
+    }
 
 private:
     size_t m_width, m_height;
+    std::vector<Cell<T>> m_values;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -17,20 +17,30 @@ vec3f cellColor(Cell<LayerType> *cell)
     return color;
 }
 
+void writeNormals(std::ostream &out, Grid &grid)
+{
+    startPPM(std::cout, grid.width(), grid.height());
+    for (size_t y = 0; y < grid.height(); ++y)
+        for (size_t x = 0; x < grid.width(); ++x)
+            writeColor(std::cout, grid.normal(x, y));
+}
+
 void writeGrid(std::ostream &out, Grid &grid)
 {
     startPPM(std::cout, grid.width(), grid.height());
-    for (auto &cell : grid.values)
-        writeColor(std::cout, cellColor(cell.get()));
+    std::for_each(grid.begin(), grid.end(), [&](auto &cell)
+                  { writeColor(std::cout, cellColor(&cell)); });
 }
 
 void addNoiseLayer(FastNoiseLite &noise, Grid &grid, LayerType type)
 {
+    // std::for_each(grid.begin(), grid.end(), [&](auto &cell)
+    //               { grid.addDepth(cell, noise.GetNoise((float)x, (float)y) * 0.5f + 0.5f, type); });
     for (int y = 0; y < grid.height(); y++)
     {
         for (int x = 0; x < grid.width(); x++)
         {
-            grid.addDepth(x, y, noise.GetNoise((float)x, (float)y) * 0.5f + 0.5f, type);
+            grid.addDepth(grid.cellIndex(x, y), noise.GetNoise((float)x, (float)y) * 0.5f + 0.5f, type);
         }
     }
 }
@@ -42,14 +52,22 @@ int main(int argc, char const *argv[])
     // Write first layer
     FastNoiseLite noise;
     noise.SetNoiseType(FastNoiseLite::NoiseType_OpenSimplex2);
+    noise.SetFrequency(0.1f);
     addNoiseLayer(noise, grid, LayerType::Rock);
     writeGrid(std::cout, grid);
 
     // Write second layer on top of first
     FastNoiseLite noise2(1783986);
     noise2.SetNoiseType(FastNoiseLite::NoiseType_OpenSimplex2);
+    noise2.SetFrequency(0.1f);
     addNoiseLayer(noise2, grid, LayerType::Soil);
     writeGrid(std::cout, grid);
+
+    writeNormals(std::cout, grid);
+
+    // std::cout
+    //     << "P3\nmax depth: " << grid.deepestCell()->totalDepth() << std::endl
+    //     << "Max Layers: " << grid.mostLayeredCell()->numLayers() << std::endl;
 
     return 0;
 }
